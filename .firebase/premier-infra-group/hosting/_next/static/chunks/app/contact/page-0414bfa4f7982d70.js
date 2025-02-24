86d925a68ac6651e958288d0585a3c2775c152f6
Srinath-exe <1406srinath@gmail.com>
2025-02-24 14:37:56 +0530

    Add new static pages, SVG assets, and update build manifest for improved functionality

---
+++ .firebase/premier-infra-group/hosting/_next/static/chunks/app/contact/page-0414bfa4f7982d70.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[977],{8548:(e,s,_)=>{Promise.resolve().then(_.bind(_,4108))}},e=>{var s=s=>e(e.s=s);e.O(0,[970,612,108,441,517,358],()=>s(8548)),_N_E=e.O()}]);

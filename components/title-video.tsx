93b1ec1cd02195af8b75c355e07f871df831c0ba
Srinath-exe <1406srinath@gmail.com>
2025-03-11 12:37:52 +0530

    Refactor TitleVideo component for improved video display and replace video file for better quality

--- components/title-video.tsx
+++ components/title-video.tsx
@@ -2,11 +2,9 @@ import React from "react";
 
 const TitleVideo = () => {
     return (
-        <div className="flex items-center justify-center w-full h-[75%]"> {/* Full screen container */}
-            <video autoPlay loop muted className="object-fill w-full h-[75%]">
-                <source src="/videos/premier.mp4" type="video/mp4" />
-            </video>
-        </div>
+        <video autoPlay loop muted className="object-cover max-w-full max-h-[75%]">
+            <source src="/videos/premier.mp4" type="video/mp4" />
+        </video>
     );
 };
 
